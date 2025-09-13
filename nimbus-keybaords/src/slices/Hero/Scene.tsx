b3126e9aa25e3104e6e58eda3bcfbe1402eba45f
Manavk2004 <mjkamdar04@gmail.com>
2025-09-13 14:13:39 -0400

    Initial keyboard render

---
+++ nimbus-keybaords/src/slices/Hero/Scene.tsx
@@ -0,0 +1,15 @@
+
+"use client"
+
+import { Keyboard } from '@/components/keyboard'
+import React from 'react'
+
+function Scene() {
+  return (
+    <group>
+        <Keyboard />
+    </group>
+  )
+}
+
+export default Scene
--- nimbus-keybaords/src/slices/Hero/index.tsx
+++ nimbus-keybaords/src/slices/Hero/index.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { Bounded } from "@/components/bounded";
+import { Canvas } from "@react-three/fiber";
+import Scene from "./Scene";
 
 /**
  * Props for `Hero`.
@@ -19,7 +23,9 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       className="text-white relative h-dvh text-shadow-black/30 text-shadow-lg blue-gradient-bg"
     >
       <div className=" hero-scene sticky pointer-events-none top-0 h-dvh w-full">
-        {/* {Canvas goes here} */}
+        <Canvas>
+          <Scene />
+        </Canvas>
       </div>
       <div className="hero-content absolute inset-x-0 top-0 h-dvh">
         <Bounded fullWidth className="absolute top-18 inset-x-0 md:top-24 md:left-[8vw]">
