06de0b14ae9a4ef406ffa70e7817cbeeb2dfc770
Manavk2004 <mjkamdar04@gmail.com>
2025-09-13 16:36:30 -0400

    Floating keypieces implemented

--- nimbus-keybaords/src/components/Keycap.tsx
+++ nimbus-keybaords/src/components/Keycap.tsx
@@ -1,10 +1,13 @@
 import * as THREE from "three";
 import { Float, useGLTF } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 type KeycapProps = {
     position?: [number, number, number];
-    rotation?: [number, number, number]
+    rotation?: [number, number, number];
+    floatSeed?: number
 }
 
 type GLTFResult = GLTF & {
@@ -14,7 +17,17 @@ type GLTFResult = GLTF & {
   materials: Record<string, unknown>;
 };
 
-export function Keycap({ position=[0,0,0], rotation=[0,0,0] }: KeycapProps) {
+export function Keycap({ position=[0,0,0], rotation=[0,0,0], floatSeed=0 }: KeycapProps) {
+
+    const group = useRef<THREE.Group>(null!)
+
+    useFrame(() => {
+        if(group.current){
+            group.current.rotation.x += floatSeed
+            group.current.rotation.y += floatSeed
+        }
+    })
+
   const { nodes } = useGLTF("/keycap.gltf") as unknown as GLTFResult;
 
   const placeholderMat = new THREE.MeshStandardMaterial({
@@ -23,16 +36,16 @@ export function Keycap({ position=[0,0,0], rotation=[0,0,0] }: KeycapProps) {
   });
 
   return (
-    <Float>
-        <group dispose={null} position={position} rotation={rotation}>
-        <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Keycap.geometry}
-            material={placeholderMat}
-            rotation={[Math.PI / 2, 0, 0]}
-            scale={10}
-        />
+    <Float rotationIntensity={1} floatIntensity={1}>
+        <group ref={group} dispose={null} position={position} rotation={rotation}>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Keycap.geometry}
+                material={placeholderMat}
+                rotation={[Math.PI / 2, 0, 0]}
+                scale={10}
+            />
         </group>
     </Float>
   );
